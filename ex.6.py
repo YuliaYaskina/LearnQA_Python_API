3f81e28426b36a624ea37f0c70da8cc1ee79f8ce
YaskinaYulia <yaskina.testing@gmail.com>
2022-01-06 22:22:18 +0300

    ex.2

--- ex.6.py
+++ ex.6.py
@@ -1,6 +1,6 @@
 import requests
 response = requests.get("https://playground.learnqa.ru/api/long_redirect")
-first_response = response.history[0]
-second_response = response
-print(first_response.url)
-print(second_response.url)
+for first_response in response.history:
+    print(first_response.url)
+last_response = response
+print(last_response.url)
